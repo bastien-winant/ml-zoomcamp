2c02f55d3b11a43c519bf2f2af63915d13467bed
bastien-winant <armand.winant@gmail.com>
2025-11-19 23:11:49 +0100

    create flask app

--- midterm/predict.py
+++ midterm/predict.py
@@ -1,36 +1,27 @@
 import pickle
+from flask import Flask, request, jsonify
 
-expedition = {
-	'year': 2006,
-	'season': 'Autumn',
-	'host': 'China',
-	'nation': 'Italy',
-	'camps': 2,
-	'totmembers': 1,
-	'tothired': 0,
-	'o2climb': False,
-	'o2descent': False,
-	'o2sleep': False,
-	'comrte': True,
-	'stdrte': True,
-	'primrte': False,
-	'sponsored': True,
-	'ascent_route': 'CHOY-NW_side',
-	'total_experience': 15,
-	'total_leadership_experience': 8,
-	'total_successes': 10,
-	'avg_climber_age': 57.0,
-	'leaders': 1,
-	'support': 0,
-	'disabled': 0,
-	'hired': 0,
-	'sherpas': 0
-}
+model_file = "criterion_gini__max_depth_20__min_samples_leaf_15.bin"
 
-file_name = "criterion_gini__max_depth_20__min_samples_leaf_15.bin"
-with open(file_name, 'rb') as f_in:
+with open(model_file, 'rb') as f_in:
 	dv, model = pickle.load(f_in)
+
+app = Flask('expedition_pred')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	expedition = request.get_json()
 	X = dv.transform([expedition])
 	pred = model.predict_proba(X)[0, 1]
+	failure_pred = pred >= .5
 
-	print(f"Expedition failure probability: {pred:.3f}")
+	result = {
+		'failure_probability': float(pred),
+		'failure': bool(failure_pred)
+	}
+
+	return jsonify(result)
+
+
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0', port=9696)
